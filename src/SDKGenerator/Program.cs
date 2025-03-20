a8a54ae043d425ce167d116923d12c6d5d54baf4
Eli <elitkramer@gmail.com>
2025-03-19 20:59:09 -0700

    .

--- src/SDKGenerator/Program.cs
+++ src/SDKGenerator/Program.cs
@@ -18,7 +18,7 @@ public static class Program
             var name = Path.GetFileNameWithoutExtension(path);
 
             // Skip most packages (for now)
-            if (name is not "Core" && name is not "Engine")
+            if (name is not "Core" && name is not "Engine" && name is not "BmGame")
             {
                 continue;
             }
@@ -33,13 +33,7 @@ public static class Program
                 pkgs.Add(pkg);
 
                 // Init package
-                pkg.InitializePackage(
-                    UnrealPackage.InitFlags.RegisterClasses
-                        | UnrealPackage.InitFlags.Construct
-                        | UnrealPackage.InitFlags.Deserialize
-                );
-
-                pkgs.Add(pkg);
+                pkg.InitializePackage(UnrealPackage.InitFlags.All);
             }
         }
 
@@ -49,13 +43,30 @@ public static class Program
             )
             .ToList();
 
+        // Deserialize classes
         Console.WriteLine($"Found {classes.Count} classes");
+        foreach (UClass classObj in classes)
+        {
+            classObj
+                .EnumerateSuper()
+                .Reverse()
+                .ToList()
+                .ForEach(super => super.BeginDeserializing());
+            classObj.BeginDeserializing();
+        }
 
-        // Test decompile pawn
-        var pawnClass = classes.First(c => c.Name == "Pawn");
+        // Test decompile Pawn CDO
+        var enginePkg = pkgs.First(pkg => pkg.PackageName == "BmGame");
+        var pawnClass = enginePkg.FindObject<UClass>("RPlayerController");
+
+        // Console.WriteLine(pawnClass.ExportTable.ClassIndex);
         pawnClass.BeginDeserializing();
+        foreach (var prop in pawnClass.EnumerateFields())
+        {
+            Console.WriteLine($"{prop.Name} ({prop.GetType().Name})");
+        }
 
-        var src = pawnClass.Decompile();
-        Console.WriteLine($"\n{src}");
+        pawnClass.Super.BeginDeserializing();
+        Console.WriteLine(pawnClass.Super.EnumerateFields().Count());
     }
 }
