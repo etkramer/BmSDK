ec2a161963e08080e62de4a63c83d5a863faade1
BitCrusader <elikramergm@gmail.com>
2025-04-01 20:09:35 -0700

    Label intrinsic classes

--- src/BmSDK.Generator/Printer/Printer.cpp
+++ src/BmSDK.Generator/Printer/Printer.cpp
@@ -28,7 +28,12 @@ void Printer::PrintClass(UClass* _class, ostream& out)
 {
 	// Print class comment
 	Printer::Indent(out) << "/// <summary>" << endl;
-	Printer::Indent(out) << "/// Class: " << _class->GetNameManaged() << "<br/>" << endl;
+	Printer::Indent(out) << "/// ";
+	if ((DWORD)_class->ClassFlags & (DWORD)EClassFlags::CLASS_Intrinsic)
+	{
+		out << "INTRINSIC ";
+	}
+	Printer::Indent(out) << "Class: " << _class->GetNameManaged() << "<br/>" << endl;
 	Printer::Indent(out) << "/// (size = " << _class->PropertiesSize << ")" << endl;
 	Printer::Indent(out) << "/// </summary>" << endl;
 
