8eaf4fa32b7900fdc101c07310d58050cb80f029
BitCrusader <elikramergm@gmail.com>
2025-05-21 19:30:32 -0700

    .

--- mods/DemoMod/DemoMod.cs
+++ mods/DemoMod/DemoMod.cs
@@ -24,7 +24,7 @@ public class DemoMod : GameMod
             {
                 Debug.Log("Entering game");
                 console.ConsoleCommand(
-                    "start batentry?Players=Playable_Batman?Area=OW,OW_A9,OW_A6,OW_A7,OW_A8,OW_R1,OW_R2,OW_E3,OW_E4,OW_A1_Static_LOD,OW_A2,OW_A3_Static_LOD,OW_A4_Static_LOD,OW_A5_Static_LOD,OW_R3,OW_E2_Static_LOD,OW_E6_Static_LOD,OW_E5_Static_LOD,OW_RE1_Static_LOD?Flags=Vertical_Slice,Demo_Riddler_Door_Switch,Map_TriggeredCityStories,Batman_ResonatorCodes,Teleport_Church_To_Museum,Demo_Ryder_Bully,Demo_Courthouse_Lock,Public_Demo?Chapters=1,1b,Z1,V1?Start=BeginVS?"
+                    "start batentry?Players=Playable_Batman?Area=Church?Flags=Vertical_Slice?Chapters=1,2?unlockall"
                 );
             });
     }
