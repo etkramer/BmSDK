14f281031d0143a1657fe5cc06adb64c20fdd386
etkramer <elikramergm@gmail.com>
2025-08-31 17:26:26 -0700

    Add BM3 offsets

--- src/BmSDK.Generator/Engine/GameOffsets.h
+++ src/BmSDK.Generator/Engine/GameOffsets.h
@@ -7,6 +7,11 @@ private:
 
 public:
 	// Globals
+#if BATMAN2
 	static const uintptr_t GObjects = 0x122E900;
 	static const uintptr_t GNames = 0x11EC5E8;
+#elif BATMAN3
+	static const uintptr_t GObjects = 0x14B1ACC;
+	static const uintptr_t GNames = 0x14B1A98;
+#endif
 };
--- src/BmSDK/FrameworkInternal/GameInfo.cs
+++ src/BmSDK/FrameworkInternal/GameInfo.cs
@@ -4,13 +4,20 @@ internal static class GameInfo
 {
     public static class GlobalOffsets
     {
+#if BATMAN2
         public const IntPtr GNames = 0x11EC5E8;
         public const IntPtr GObjObjects = 0x122E900;
         public const IntPtr GError = 0x117BF48;
+#elif BATMAN3
+        public const IntPtr GNames = 0x14B1A98;
+        public const IntPtr GObjObjects = 0x14B1ACC;
+        public const IntPtr GError = 0x13EE784;
+#endif
     }
 
     public static class FuncOffsets
     {
+#if BATMAN2
         // app (global)
         public const IntPtr AppRealloc = 0x1EC10;
 
@@ -38,6 +45,35 @@ internal static class GameInfo
 
         // FString (local)
         public const IntPtr StringCtor = 0x4650;
+#elif BATMAN3
+        // app (global)
+        public const IntPtr AppRealloc = 0x1EF50;
+
+        // UObject (static)
+        public const IntPtr StaticFindObject = 0x86950;
+        public const IntPtr StaticConstructObject = 0x8EE80;
+        public const IntPtr LoadPackage = 0x92210;
+
+        // UObject (local)
+        public const IntPtr ProcessEvent = 0x428A0;
+        public const IntPtr ProcessInternal = 0x42730;
+        public const IntPtr CallFunction = 0x47E10;
+        public const IntPtr AddObject = 0x834C0;
+        public const IntPtr ConditionalDestroy = 0x841F0;
+        public const IntPtr FindFunction = 0x84390;
+
+        // UClass (local)
+        public const IntPtr GetDefaultObject = 0x449C0;
+
+        // UWorld (local)
+        public const IntPtr SpawnActor = 0x329D30;
+
+        // FName (local)
+        public const IntPtr NameInit = 0x85180;
+
+        // FString (local)
+        public const IntPtr StringCtor = 0x4FF0;
+#endif
     }
 
     public static class MemberOffsets
