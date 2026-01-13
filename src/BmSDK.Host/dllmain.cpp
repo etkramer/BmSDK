3cbeddf05c7f5a88c8e2f289d61e959b49a39d69
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-01-13 23:19:20 +0100

    Revert "fix(SDK): Don't immediately close hostfxr_handle"
    This reverts commit 0e0776fe74d034766866f6cf471408dc57018ab0.

--- src/BmSDK.Host/dllmain.cpp
+++ src/BmSDK.Host/dllmain.cpp
@@ -28,8 +28,6 @@ HostGetDelegateFn hostGetDelegateFn = nullptr;
 HostCloseFn hostCloseFn = nullptr;
 HostLoadAssemblyFn hostLoadAssemblyFn = nullptr;
 
-hostfxr_handle cxt = nullptr;
-
 // Filesystem helpers
 static path get_plugin_path()
 {
@@ -77,6 +75,7 @@ static HostLoadAssemblyFn get_dotnet_load_assembly(const char_t* config_path)
 {
 	// Load .NET Core
 	void* hostLoadAssemblyFn = nullptr;
+	hostfxr_handle cxt = nullptr;
 	int rc = hostInitFn(config_path, nullptr, &cxt);
 	if (rc != 0 || cxt == nullptr)
 	{
@@ -93,6 +92,7 @@ static HostLoadAssemblyFn get_dotnet_load_assembly(const char_t* config_path)
 	if (rc != 0 || hostLoadAssemblyFn == nullptr)
 		std::cerr << "Get delegate failed: " << std::hex << std::showbase << rc << std::endl;
 
+	hostCloseFn(cxt);
 	return (HostLoadAssemblyFn)hostLoadAssemblyFn;
 }
 
@@ -157,11 +157,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 		run_bmsdk();
 		break;
 	case DLL_THREAD_ATTACH:
-		break;
 	case DLL_THREAD_DETACH:
-		break;
 	case DLL_PROCESS_DETACH:
-		hostCloseFn(cxt);
 		break;
 	}
 	return TRUE;
