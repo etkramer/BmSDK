b87df95b7cc6a47d1db6a245dbfaa1c26f7833da
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-01-19 00:34:44 +0100

    feat(SDK): Add EngineTick detour for syncing
    Added a detour that is run on `FEngineLoop::Tick()`.
    We are using it to call the scheduled tasks on the main thread. Tick is the function that is executed the most quickly and reliably, therefore, it is fit for this task.

--- src/BmSDK/FrameworkInternal/EngineSynchronizationContext.cs
+++ src/BmSDK/FrameworkInternal/EngineSynchronizationContext.cs
@@ -4,7 +4,14 @@ namespace BmSDK.Framework;
 
 public sealed class EngineSynchronizationContext : SynchronizationContext
 {
+    private static readonly EngineSynchronizationContext s_instance = new();
+    internal static EngineSynchronizationContext Instance => s_instance;
+
+    internal static void Init() => SetSynchronizationContext(s_instance);
+
     private readonly ConcurrentQueue<(SendOrPostCallback callback, object? state)> _queue = new();
+
+    private EngineSynchronizationContext() { }
 
     public override void Post(SendOrPostCallback d, object? state)
         => _queue.Enqueue((d, state));
