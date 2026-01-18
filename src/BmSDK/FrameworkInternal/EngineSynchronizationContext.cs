d42258fc6216b3e11e2bcdb67bc7b7fb7f6558e7
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-01-18 23:30:52 +0100

    feat(SDK): Add custom SyncContext class
    UI APIs for C# provide a SynchronizationContext implementation to get back on the UI thread. In console apps (like BmSDK) we need to implement it ourselves. This is a simple one that just appends calls to `Post()` to a queue which is then supposed to be worked off later in the Engine's tick function.

---
+++ src/BmSDK/FrameworkInternal/EngineSynchronizationContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace BmSDK.Framework;
+
+public sealed class EngineSynchronizationContext : SynchronizationContext
+{
+    private readonly ConcurrentQueue<(SendOrPostCallback callback, object? state)> _queue = new();
+
+    public override void Post(SendOrPostCallback d, object? state)
+        => _queue.Enqueue((d, state));
+
+    internal void ExecutePending()
+    {
+        while (_queue.TryDequeue(out var work))
+        {
+            work.callback(work.state);
+        }
+    }
+}
