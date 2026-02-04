79c90e880a59f43ab10059174b0aba1be9dce1da
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-02-05 00:11:25 +0100

    chore: Update redirect example script
    I updated the script to showcase the difference between local and global redirects.

--- bin/BmGame/Scripts/RedirectTestScript.cs
+++ bin/BmGame/Scripts/RedirectTestScript.cs
@@ -6,22 +6,56 @@ public static class GlobalRedirects
     [Redirect(typeof(RPawnPlayer), nameof(RPawnPlayer.ExperienceAwarded))]
     public static void ExperienceAwardedRedirect(RPawnPlayer self, int xp, int teethXp)
     {
-        Debug.Log("Experiencing less...");
-        self.ExperienceAwarded(xp / 10, teethXp / 10);
+        self.ExperienceAwarded(xp, teethXp);
+        self.PlayerController.HudMovieNew.GeneralMovie.SetInfoText(
+            "Default regeneration",
+            (int) RGFxMovieHudExtendable.JustifyText.JT_Center,
+            0x00FF00
+        );
     }
 }
 
-[ScriptComponent(AutoAttach = true)]
-public sealed class XRayActivatorComponent : ScriptComponent<RPawnPlayer>
+[ScriptComponent]
+public sealed class RegenReductionComponent : ScriptComponent<RPawnPlayer>
 {
-    public override void OnAttach() => Debug.Log("XRayActivatorComponent attached to player pawn!");
+    public override void OnAttach() => Debug.Log("RegenReductionComponent attached to player pawn!");
 
-    public override void OnDetach() => Debug.Log("XRayActivatorComponent attached to player pawn!");
+    public override void OnDetach() => Debug.Log("RegenReductionComponent detached to player pawn!");
 
+    // Local redirect which only applies to actors with the script component attached
+    // Overrides the global redirect
     [ComponentRedirect(nameof(RPawnPlayer.ExperienceAwarded))]
     public void ExperienceAwardedRedirect(int xp, int teethXp)
     {
-        Owner.PlayerController.bInvestigateMode = true;
-        Owner.ExperienceAwarded(xp * 10, teethXp * 10);
+        Owner.ExperienceAwarded(xp / 10, teethXp / 10);
+        Owner.PlayerController.HudMovieNew.GeneralMovie.SetInfoText(
+            "Reduced regeneration",
+            (int) RGFxMovieHudExtendable.JustifyText.JT_Center,
+            0xFF0000
+        );
+    }
+}
+
+[Script]
+public class RedirectTestScript : Script
+{
+    public override void OnKeyDown(Keys key)
+    {
+        if (key != Keys.N)
+        {
+            return;
+        }
+
+        var rpp = Game.GetPlayerPawn();
+        // You may only attach one ScriptComponent instance of a given type
+        // Therefore, we can check if the SC is attached using generics
+        if (!rpp.HasScriptComponent<RegenReductionComponent>())
+        {
+            rpp.AttachScriptComponent<RegenReductionComponent>();
+        }
+        else
+        {
+            rpp.DetachScriptComponent<RegenReductionComponent>();
+        }
     }
 }
