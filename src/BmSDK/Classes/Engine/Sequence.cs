f0e154bec941cd3f21ace309aba3d7d205382ebb
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-03-11 18:42:36 +0100

    docs: add XML comments for new functions

--- src/BmSDK/Classes/Engine/Sequence.cs
+++ src/BmSDK/Classes/Engine/Sequence.cs
@@ -2,6 +2,13 @@ namespace BmSDK.Engine;
 
 public partial class Sequence
 {
+    /// <summary>
+    /// Retrieves all sequence objects of a given type in the current sequence
+    /// and optionally its nested sequences as well.
+    /// </summary>
+    /// <typeparam name="TClass">SequenceObject type to filter for</typeparam>
+    /// <param name="recursive">Whether to search nested sequences too</param>
+    /// <returns>Collection of sequence objects of the specified type in the sequence</returns>
     public IEnumerable<TClass> FindSeqObjectsByClass<TClass>(bool recursive)
         where TClass : SequenceObject
     {
@@ -15,6 +22,14 @@ public partial class Sequence
         return seqObjs.Concat(NestedSequences.SelectMany(s => s.FindSeqObjectsByClass<TClass>(true)));
     }
 
+    /// <summary>
+    /// Retrieves all currently active sequence operations of a given type in
+    /// the current sequence and optionally its nested sequences as well.
+    /// This is useful when searching for currently active SeqActs.
+    /// </summary>
+    /// <typeparam name="TClass">SequenceOp type to filter for</typeparam>
+    /// <param name="recursive">Whether to search nested sequences too</param>
+    /// <returns>Collection of sequence ops of the specified type in the sequence</returns>
     public IEnumerable<TClass> FindActiveSeqOpsByClass<TClass>(bool recursive)
         where TClass : SequenceOp
     {
