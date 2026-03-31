088884821c082738decf0f150eb7104e5ee5693a
etkramer <elikramergm@gmail.com>
2026-03-31 15:36:20 -0700

    feat: add Actor.IsSpawned

--- src/BmSDK/Classes/Engine/Actor.cs
+++ src/BmSDK/Classes/Engine/Actor.cs
@@ -4,6 +4,13 @@ namespace BmSDK.Engine;
 
 public partial class Actor
 {
+    /// <summary>
+    /// Returns true if this actor is part of an active level in the current world.
+    /// Returns false for actors that are in memory from a <see cref="Game.LoadPackage"/> call
+    /// but whose level has not been streamed into the world.
+    /// </summary>
+    public bool IsSpawned => Outer?.Outer is World;
+
     /// <inheritdoc cref="GameObject.Clone"/>
     public new Actor Clone() => Game.SpawnActor(Class, Location, Rotation, this, Owner);
 }
